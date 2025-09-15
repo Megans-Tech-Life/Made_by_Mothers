c8d5b06f6e6428aa59f4f92bb511739ea60cc586
Megan Jakims <meganjakims1@gmail.com>
2025-09-15 09:19:09 -0400

    fix router issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouterRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./components/App/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Made_by_Mothers-frontend">
+    <HashRouterRouter basename="/Made_by_Mothers-frontend">
       <App />
-    </BrowserRouter>
+    </HashRouterRouter>
   </StrictMode>
 );
